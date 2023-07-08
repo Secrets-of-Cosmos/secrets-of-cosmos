d0d4bab41d67b43671877f47531e437986702c26
Emir Soyturk <emirsytrk@gmail.com>
2023-07-08 17:50:47 +0300

    controll

--- Assets/Code/Scripts/SpaceShipController.cs
+++ Assets/Code/Scripts/SpaceShipController.cs
@@ -7,11 +7,17 @@ public class SpaceShipController : MonoBehaviour
     public float sideThrust = 5f;
     public float pitchTorque = 50f;
     public float rollTorque = 30f;
+    public float thrustForce = 20f;
+
+    public GameObject planet;
+    private bool lockedToPlanet = false;
 
     private Rigidbody rb;
     private CockpitController controls;
     private Vector2 moveInput;
     private Vector2 rotateInput;
+    private float thrustInput;
+
     private bool controlsEnabled = true;
 
     private void Awake()
@@ -24,6 +30,9 @@ public class SpaceShipController : MonoBehaviour
 
         controls.Player.rotate.performed += ctx => rotateInput = ctx.ReadValue<Vector2>();
         controls.Player.rotate.canceled += ctx => rotateInput = Vector2.zero;
+
+        controls.Player.thrust.performed += ctx => thrustInput = ctx.ReadValue<float>();
+        controls.Player.thrust.canceled += ctx => thrustInput = 0f;
     }
 
     private void OnEnable()
@@ -42,6 +51,14 @@ public class SpaceShipController : MonoBehaviour
         {
             MoveShip();
         }
+        if(Input.GetKeyDown("r"))
+        {
+            lockedToPlanet = !lockedToPlanet;
+        }
+        if (lockedToPlanet)
+        {
+            LockToPlanet(planet);
+        }
     }
 
     void MoveShip()
@@ -52,11 +69,24 @@ public class SpaceShipController : MonoBehaviour
         Vector3 pitchTorqueVec = transform.right * -rotateInput.y * pitchTorque;
         Vector3 rollTorqueVec = transform.forward * -rotateInput.x * rollTorque;
 
+        Vector3 thrustVector = transform.up * thrustInput * thrustForce;
+
+
         rb.AddForce(forwardThrustVec, ForceMode.Force);
         rb.AddForce(sideThrustVec, ForceMode.Force);
 
         rb.AddTorque(pitchTorqueVec, ForceMode.Force);
         rb.AddTorque(rollTorqueVec, ForceMode.Force);
+
+        rb.AddForce(thrustVector, ForceMode.Force);
+    }
+
+    void LockToPlanet(GameObject planet)
+    {
+        Vector3 directionToPlanet = (planet.transform.position - transform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(directionToPlanet);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2f);
     }
 
     public void ShakeShip()
