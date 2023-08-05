b0673a8acac6897410039bc69fcc702f0e65dc08
Emir Soyturk <emirsytrk@gmail.com>
2023-08-05 19:20:13 +0300

    Collect and Drop Rock with Perseverance Rover

--- Assets/Code/Scripts/Rover/Rover.cs
+++ Assets/Code/Scripts/Rover/Rover.cs
@@ -1,89 +1,46 @@
 using UnityEngine;
-using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Rover : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rotationSpeed = 30f;
-    public float groundRayLength = 1.5f;
-    public Transform groundRayOrigin;
-    public Transform[] wheelTransforms;
-    public LayerMask groundLayer;
-    public LayerMask obstacleLayer; // Engelleri tespit etmek için
-    public float timeToChangeDirection = 2f;
-    private float directionChangeInterval;
-    private float surfaceAlignmentTime = 1f; // Yüzeyle uyum sağlama süresi (saniye)
-    private Quaternion targetRotation;
-    private Rigidbody rb;
+    public float motorTorque = 200f;
+    public float maxSteerAngle = 30f;
+    public WheelCollider[] frontWheelColliders;
+    public WheelCollider[] rearWheelColliders;
+    public Transform[] wheelMeshes;
 
-    void Start()
+    public float randomRotationInterval = 5f;
+    private float randomRotationTimer = 0f;
+    private float steerAngle = 0f;
+
+    private void FixedUpdate()
     {
-        rb = GetComponent<Rigidbody>();
-        targetRotation = transform.rotation; // Başlangıç dönüşünü hedef olarak ayarla
-    }
-
-    void Update()
-    {
-
         Move();
-        if (transform.rotation != targetRotation)
+
+        randomRotationTimer += Time.deltaTime;
+
+        if (randomRotationTimer >= randomRotationInterval)
         {
-            // LeanTween.rotate(gameObject, targetRotation.eulerAngles, surfaceAlignmentTime);
-            // Yüzey normali ile uyumlu hale getiriliyor
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, surfaceAlignmentTime * Time.deltaTime);
+            randomRotationTimer = 0f;
+            steerAngle = Random.Range(-maxSteerAngle, maxSteerAngle);
         }
     }
 
-    void Move()
+    private void Move()
     {
-        // RotateRandomly();
-        AlignToSurface();
-        SpeedControl();
-        WheelRotation();
-    }
+        float motorForce = motorTorque;
 
-    void RotateRandomly()
-    {
-        directionChangeInterval -= Time.deltaTime;
-        if (directionChangeInterval < 0)
+        for (int i = 0; i < frontWheelColliders.Length; i++)
         {
-            directionChangeInterval = timeToChangeDirection;
-            // Rastgele bir yön seç
-            float angle = Random.Range(-90f, 90f);
-            targetRotation *= Quaternion.AngleAxis(angle, Vector3.up);
+            frontWheelColliders[i].steerAngle = steerAngle;
+        }
+
+        for (int i = 0; i < rearWheelColliders.Length; i++)
+        {
+            rearWheelColliders[i].motorTorque = motorForce;
+            wheelMeshes[i + 2].Rotate(rearWheelColliders[i].rpm / 60 * 360 * Time.deltaTime, 0, 0);
         }
     }
 
-    void WheelRotation()
-    {
-        if (rb.velocity.magnitude > 0)
-        {
 
-            foreach (Transform wheel in wheelTransforms)
-            {
-                wheel.Rotate(-rb.velocity.magnitude * Time.deltaTime * 50f, 0f, 0f);
-            }
-        }
-    }
-
-    void SpeedControl()
-    {
-        if (rb.velocity.magnitude < speed)
-        {
-            Vector3 force = transform.forward * speed;
-            rb.AddForce(force, ForceMode.VelocityChange);
-        }
-    }
-
-    void AlignToSurface()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(groundRayOrigin.position, -groundRayOrigin.up, out hit, groundRayLength, groundLayer))
-        {
-            Debug.Log("Yüzey normali ile uyumlu hale getiriliyor");
-            // Yüzey normali ile uyumlu hale getiriliyor
-            Quaternion newRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-            targetRotation = newRotation;
-        }
-    }
 }
